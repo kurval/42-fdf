6e301fdd6e85b0725d5ecfb660333059f2a2be42
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-11-01 13:26:08 +0200

    adding mouse position

--- sources/key_controls.c
+++ sources/key_controls.c
@@ -6,26 +6,12 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/26 17:53:25 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/11/01 12:43:14 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/11/01 13:24:45 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 #include "../includes/error.h"
-
-static void set_shift(int keycode, t_map *map_info)
-{
-    if (keycode == KEY_LEFT)
-        SHIFT_X -= 20;
-    else if (keycode == KEY_RIGHT)
-        SHIFT_X += 20;
-    else if (keycode == KEY_UP)
-        SHIFT_Y -= 20;
-    else if (keycode == KEY_DOWN)
-        SHIFT_Y += 20;
-    mlx_clear_window(MLX, WIN);
-    expose_hook(map_info);
-}
 
 static void set_altitude(int keycode, t_map *map_info)
 {
@@ -56,8 +42,6 @@ int    press_key(int keycode, t_map *map_info)
         set_projections(keycode, map_info);
     else if (keycode == 97 || keycode == 115)
         set_altitude(keycode, map_info);
-    else if (keycode >= KEY_LEFT && keycode <= KEY_DOWN)
-        set_shift(keycode, map_info);
     else if (keycode == 100)
     {
         reset_controls(map_info);
--- sources/mouse_controls.c
+++ sources/mouse_controls.c
@@ -6,7 +6,7 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 12:39:59 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/11/01 12:58:26 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/11/01 13:24:52 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,20 @@ static void set_zoom(int button, int x, int y, t_map *map_info)
     expose_hook(map_info);
 }
 
+int    set_position(int button, int x, int y, t_map *map_info)
+{
+    SHIFT_X = x;
+    SHIFT_Y = y;
+    mlx_clear_window(MLX, WIN);
+    expose_hook(map_info);
+    return (1);
+}
+
 int    press_mouse(int button, int x, int y, t_map *map_info)
 {
     if (button == 4 || button == 5)
         set_zoom(button, x, y, map_info);
+    else if (button == 1)
+        set_position(button, x, y, map_info);
     return (1);
 }
