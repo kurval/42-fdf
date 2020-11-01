a1bb790c1d92d19902e67279f26c82eaf30dd66c
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-11-01 15:43:08 +0200

    refactoring

--- sources/mouse_controls.c
+++ sources/mouse_controls.c
@@ -6,14 +6,14 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 12:39:59 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/11/01 13:24:52 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/11/01 15:38:16 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 #include "../includes/error.h"
 
-static void set_zoom(int button, int x, int y, t_map *map_info)
+static void set_zoom(int button, t_map *map_info)
 {
     if (button == 4)
         map_info->controls->zoom += 1;
@@ -23,7 +23,7 @@ static void set_zoom(int button, int x, int y, t_map *map_info)
     expose_hook(map_info);
 }
 
-int    set_position(int button, int x, int y, t_map *map_info)
+static int    set_position(int x, int y, t_map *map_info)
 {
     SHIFT_X = x;
     SHIFT_Y = y;
@@ -35,8 +35,8 @@ int    set_position(int button, int x, int y, t_map *map_info)
 int    press_mouse(int button, int x, int y, t_map *map_info)
 {
     if (button == 4 || button == 5)
-        set_zoom(button, x, y, map_info);
+        set_zoom(button, map_info);
     else if (button == 1)
-        set_position(button, x, y, map_info);
+        set_position(x, y, map_info);
     return (1);
 }
