bbd175dc7a58d34d323841f4a09920536c5cd777
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-11-01 12:59:07 +0200

    refactoring

--- sources/mouse_controls.c
+++ sources/mouse_controls.c
@@ -6,7 +6,7 @@
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/01 12:39:59 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/11/01 12:47:30 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/11/01 12:58:26 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,12 @@
 
 static void set_zoom(int button, int x, int y, t_map *map_info)
 {
-    x = map_info->pixels[0][0]->px;
-    y = map_info->pixels[0][0]->py;
-    if (x >= 0 && x <= MAP_WIDTH && y >= 0 && y <= MAP_HEIGHT)
-	{
-        if (button == 4)
-            map_info->controls->zoom += 1;
-        if (button == 5)
-            map_info->controls->zoom -= 1;
-        mlx_clear_window(MLX, WIN);
-        expose_hook(map_info);
-    }
+    if (button == 4)
+        map_info->controls->zoom += 1;
+    if (button == 5)
+        map_info->controls->zoom -= 1;
+    mlx_clear_window(MLX, WIN);
+    expose_hook(map_info);
 }
 
 int    press_mouse(int button, int x, int y, t_map *map_info)
