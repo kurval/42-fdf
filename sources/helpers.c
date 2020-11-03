c7ed27393c055b327dfed14551e8f12118051895
Valtteri Kurkela <valtteri.kurkela@outlook.com>
2020-11-03 10:57:39 +0200

    refactoring

--- sources/helpers.c
+++ sources/helpers.c
@@ -1,16 +1,42 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   errors.c                                           :+:      :+:    :+:   */
+/*   helpers.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vkurkela <vkurkela@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/20 10:35:43 by vkurkela          #+#    #+#             */
-/*   Updated: 2020/10/20 10:37:59 by vkurkela         ###   ########.fr       */
+/*   Updated: 2020/11/03 10:56:06 by vkurkela         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
+
+static void    free_map(t_map *map_info)
+{
+    int y;
+	int x;
+
+	y = 0;
+	while (map_info->pixels[y])
+	{	
+		x = 0;
+		while (PIX)
+        {
+			free(PIX);
+			x++;
+		}
+        free(map_info->pixels[y]);
+		y++;
+	}
+    free(map_info->pixels);
+}
+
+void	free_all(t_map *map_info)
+{
+	free(map_info->controls);
+	free_map(map_info);
+}
 
 void    print_error(char *error_msg)
 {
